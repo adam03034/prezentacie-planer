fe248e40affa19d5f1b8a3aaab20dab3531739b2
Adam Wirth <adamwirth@MacBook-Air-uzivatela-Adam.local>
2025-03-23 13:26:48 +0100

    Fix GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/adam03034/prezentacie-planer'  // Nahraďte 'název-repozitáře' skutečným názvem vašeho GitHub repozitáře
+    ? '/prezentacie-planer/'
     : '/'
 }
